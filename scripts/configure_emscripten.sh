1ed74577d8c538626c154381a385104441f846ca
gabkhanfig <40899981+gabkhanfig@users.noreply.github.com>
2025-10-08 21:58:13 -0400

    Update configure_emscripten.sh

--- scripts/configure_emscripten.sh
+++ scripts/configure_emscripten.sh
@@ -1,5 +1,12 @@
+#!/bin/bash 
+
+echo install
 ./emscripten/emsdk install latest
+echo activate
 ./emscripten/emsdk activate latest
+echo source
 source ./emscripten/emsdk_env.sh
+echo emcmake
 emcmake cmake -S . -B build
+echo build
 cmake --build build
